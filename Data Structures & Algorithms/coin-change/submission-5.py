eea24a4223bcf7d4d0cf5bd16f19aa92bd031cb2
samarth212 <95194221+samarth212@users.noreply.github.com>
2026-07-24 14:12:58 -0400

    Add: coin-change - submission-5

---
+++ Data Structures & Algorithms/coin-change/submission-5.py
@@ -0,0 +1,46 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        '''
+        [1,5,10], amount = 12
+
+        dfs(remaining) -> return depth 
+         - base cases: remaining = 0, < 0
+
+        dfs(12) = min(dfs(11), dfs(7), dfs(2))
+
+        loop through choices
+        run dfs(amount-choice = remaining)
+            if remaining = 0, return 1
+            if remaining < 0, return infinity
+
+            return 1 + min(remaining choices)
+
+        return dfs(amount)
+
+        '''
+
+        dp = {}
+        
+        def dfs(remaining):
+            if remaining == 0:
+                return 0
+            if remaining < 0:
+                return float('inf')
+
+            if remaining in dp:
+                return dp[remaining]
+
+            minDepth = float('inf')
+            for i in coins:
+                minDepth = min(dfs(remaining-i), minDepth)
+            
+            dp[remaining] = 1 + minDepth
+            return dp[remaining]
+
+        res = dfs(amount)
+        if res == float('inf'):
+            return -1
+        return res
+
+       
