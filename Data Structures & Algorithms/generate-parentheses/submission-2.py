7cb53f9f3201dfdf161fc01ecd4c481d7a6a5542
samarth212 <95194221+samarth212@users.noreply.github.com>
2026-07-29 10:52:21 -0400

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        res = set()
+        path = ''
+        stack = []
+        opened = 0
+        closed = 0
+
+        def dfs():
+            nonlocal path
+            if len(path) == n*2:
+                if not stack:
+                    res.add(path[:])
+                return
+
+            path += '('
+            stack.append('(')
+            dfs()
+            path = path[:-1]
+            stack.pop()
+     
+
+            if stack:
+                path += ')'
+                stack.pop()
+                dfs()
+                path = path[:-1]
+                stack.append('(')
+
+        dfs()
+        return list(res)
+
+            
+            
+
+        
