0074017259647b0e8f29257560efb7c787eb1899
samarth212 <95194221+samarth212@users.noreply.github.com>
2026-07-30 09:56:54 -0400

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+
+        '''
+        
+        [1,2,4,2,3,5,3,4]
+
+        [1,2,4,2,3,5,3,4]
+
+
+
+
+
+
+
+
+        '''
+        
