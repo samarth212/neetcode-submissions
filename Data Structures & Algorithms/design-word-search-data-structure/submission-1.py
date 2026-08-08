c48403c533c88c73dcb02105976eefb2871b9df1
samarth212 <95194221+samarth212@users.noreply.github.com>
2026-08-08 14:04:44 -0400

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,57 @@
+class Node():
+    def __init__(self):
+        self.children = {}
+        self.end = False
+        
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node()
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Node()
+            curr = curr.children[c]
+        
+        curr.end = True
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for i, c in enumerate(word):
+            if c != '.':
+                if c not in curr.children:
+                    return False
+                curr = curr.children[c]
+            else:
+                for code in range(97, 123):
+                    temp = chr(code) + word[i+1:]
+                    if self.searchFromNode(temp, curr):
+                        return True
+                return False    
+        
+        return curr.end
+    
+    def searchFromNode(self, word, node):
+        curr = node
+
+        for i, c in enumerate(word):
+            if c != '.':
+                if c not in curr.children:
+                    return False
+                curr = curr.children[c]
+            else:
+                for code in range(97, 123):
+                    temp = chr(code) + word[i+1:]
+                    if self.searchFromNode(temp, curr):
+                        return True
+                return False
+
+            
+        return curr.end
+
